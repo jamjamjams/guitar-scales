8e997f78494a587dc1a22bac45c8e4443e0035f8
LiamOsler <osler.liam@gmail.com>
2023-12-21 16:51:18 -0400

    boilerplate

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <h1>This is the Navbar, a bar for navs</h1>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Scale/Scale.js
@@ -0,0 +1,11 @@
+import './Scale.css';
+
+function Scale() {
+  return (
+    <div className="Scale">
+        <h1>This is my first react app</h1>
+    </div>
+  );
+}
+
+export default Scale;
